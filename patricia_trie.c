97bfa34e1776ad98d8696440a62c2b01a03f70c6
virtuecho <jasontse0522@gmail.com>
2024-09-06 14:21:59 +1000

    Fix prefix_bits

--- patricia_trie.c
+++ patricia_trie.c
@@ -7,7 +7,7 @@ patricia_node_t* insert_patricia(patricia_node_t* root, char* key, data_t* data)
 	// create a new patricia trie
 	if (root == NULL) {
 		patricia_node_t* node = malloc(sizeof(patricia_node_t));
-		node->prefix_bits = strlen(key) * BITS_PER_BYTE;
+		node->prefix_bits = (strlen(key) + 1) * BITS_PER_BYTE;
 		node->prefix = strdup(key);
 		node->data = data;
 		node->branchA = NULL;
